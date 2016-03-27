a917cc521a259125784ab752f36e30cac77f46d1
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2016-03-27 19:48:56 -0300

    Commit Inicial

---
+++ android/src/com/leonardo/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.leonardo.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.leonardo.game.TheLegendOfGisclene;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TheLegendOfGisclene(), config);
+	}
+}
